0b99141dfab1405e0a16fdc1f8d3174d334e2a21
Storm Van Soldt <13080497+stormvansoldt@users.noreply.github.com>
2022-02-02 14:11:09 -0700

    Initial commit

---
+++ templates/temp-index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    body { 
+      background-repeat: no-repeat;
+      background-color: #99ffff;
+    }
+    
+    img {
+      width: 300px;
+      height: auto;
+    }
+
+    input {
+      text-align: center;
+    }
+
+    table {
+      border: 1;
+      border-collapse: collapse;
+      color: black;
+      border: 2px solid black;
+    }
+
+    td {
+    }
+
+    .footer {
+      position: fixed;
+      text-align: center;
+      bottom: 0px;
+      width: 100%;
+    }
+  </style>
+</head>
+<body>
+  <form action="submit.php" method="POST">
+    <!-- INSERT CONTENT HERE -->
+  </form>
+  <div class="footer">
+    <img src="darksign_logo.png">
+  </div>
+</body>
+</html>
