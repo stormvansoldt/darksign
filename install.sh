17fe4d3dcd3e628e3c52b469e5ec0bc62e3db001
Storm Van Soldt <13080497+stormvansoldt@users.noreply.github.com>
2022-02-02 13:23:37 -0700

    initial commit

---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+## Update everything and reboot
+apt update && apt upgrade -y
+rpi-update
+reboot now
+
+## Install packages
+apt install vim apache2 php php-curl sqlite3 php7.3-sqlite3 git rsync smbclient cifs-utils
+mkdir /root/bin && cd /root/bin && git clone https://github.com/seamusdemora/RonR-RPi-image-utils.git
+mv /root/bin/RonR-RPi-image-utils/image-* /usr/local/bin/
+chmod a+x /usr/local/bin/image-* 
+
+## Enable the sqlite3 extension in /etc/php/7.3/apache2/php.ini
+PHP_INI=/etc/php/7.3/apache2/php.ini
+if [ -f "$FILE" ]; then
+	sed -i 's/;extension=sqlite3/extension=sqlite3/g' /etc/php/7.3/apache2/php.ini
+	sed -i 's/;extension=pdo_sqlite/extension=sqlite/g' /etc/php/7.3/apache2/php.ini
+	service apache2 restart
+	echo "SQLite3 extension has been enabled in $FILE"
+else
+	echo "ERROR: $FILE not found"
+fi
+
+## Remove Apache's default index.html
+rm /var/www/html/index.html
+
+## Copy premade PiSignage files to /home/pi/media
+# custom_layout-file.html
+# background_image.png
+# __mainpl.json
+
+## Copy premade Apache files to /var/www/html
+# index.html/index.php
+# upload.php
+
+## Change some file/folder permissions. Do this after all files have been uploaded and modified
+useradd pi www-data
+chown -R www-data:www-data /var/www
+chmod -R g+rwX /var/www
+chmod 666 /home/pi/media/__mainpl.json
+
+## Create darkuser
+#adduser --gecos "" --disabled-password darkuser
+#adduser -aG sudo darkuser
+#chpasswd <<< "darkuser:stormisprettycool"
