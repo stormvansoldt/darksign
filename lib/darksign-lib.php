401d704f49913dcabc52d427f289c9d2c7bb4de8
Storm Van Soldt <13080497+stormvansoldt@users.noreply.github.com>
2022-02-03 11:37:53 -0700

    Removed debugging variables to make code more readable

--- lib/darksign-lib.php
+++ lib/darksign-lib.php
@@ -6,30 +6,24 @@
 
 function restartPiSi()
 {
-    $addr       = $_SERVER['SERVER_ADDR'];
-    $base_url   = 'http://'. $addr .':8000/api/play/playlists/mainpl';
-    $action     = ['stop' => true,];
-    $curl       = curl_init();
+  $base_url   = 'http://'. $_SERVER['SERVER_ADDR'] .':8000/api/play/playlists/mainpl';
+  $curl       = curl_init();
 
-    // set curl options for POST request
-    curl_setopt($curl, CURLOPT_URL, $base_url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($action));
-    curl_setopt($curl, CURLOPT_HTTPHEADER, [
-      'Accept: application/json',
-      'Authorization: Basic cGk6cGk=',
-      'Content-Type: application/json'
-    ]);
+  // set curl options for POST request
+  curl_setopt($curl, CURLOPT_URL, $base_url);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($curl, CURLOPT_POST, true);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode(['stop' => true,]));
+  curl_setopt($curl, CURLOPT_HTTPHEADER, [
+    'Accept: application/json',
+    'Authorization: Basic cGk6cGk=',
+    'Content-Type: application/json'
+  ]);
 
-    // Execute the request and save the response. 
-    $json_response  = curl_exec($curl);
-    $stop_response  = json_decode($json_response, true);
-    $action         = ['play' => true,];
-
-    sleep(3);
-
-    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($action));
-
-    $play_response  = curl_exec($curl);
+  // Execute the request and save the response. 
+  curl_exec($curl);
+  sleep(1);
+  
+  curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode(['play' => true,]));
+  curl_exec($curl);
 }
